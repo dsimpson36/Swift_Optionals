a75ba06078e3cc5ae6f9a91946023d16b2afe471
Deven <d_simpson16@hotmail.com>
2019-07-08 09:43:51 -0700

    worked on answering the questions

--- Optionals Play.playground/Contents.swift
+++ Optionals Play.playground/Contents.swift
@@ -15,8 +15,8 @@ var myOptionalString:String?
 print(myOptionalString) // this line should give a warning
 
 // MARK: Unwrapping an Optional converts it to the base data type
-myOptionalString = "World."
-print(myOptionalString) // this line should give a warning
+myOptionalString = "world."
+print(myOptionalString!) // this line should give a warning
 // Rewrite the line above to use myOptionalString! to "implicitly unwrap" the Optional. This will convert the Optional to a String and remove the warning
 
 //TODO: remove the comment on the next line and fix the ERROR that it causes
@@ -24,14 +24,14 @@ print(myOptionalString) // this line should give a warning
 
 print(aString.uppercased())
 //TODO: remove the comment on the next line and fix the ERROR that it causes
-//print(myOptionalString.uppercased())
+print(myOptionalString?.uppercased())
 
 // MARK: Optional Binding (if let)
-var anotherString:String?
+var anotherString:String? = "hi"
 
-if let bString = anotherString?.uppercased(){
-    //TODO: remove the comment on the next line and fix the ERROR that it causes using the binding variable bString
-    //print("Say \(aString) \(anotherString.uppercased()) with Optional Binding" )
+if let bString = anotherString {
+
+    print("Say \(aString) \(bString.uppercased()) with Optional Binding" )
 }
 // Initialize anotherString="Hello" just before the Optional Binding
 
@@ -39,6 +39,7 @@ if let bString = anotherString?.uppercased(){
 // MARK: Nil-Coalescing Operator ??
 // TODO: Use the ?? operator instead of the Optional Binding (if let) above to print out "Hello WORLD"
 var cString:String = anotherString ?? "World"
+print("cString: \(cString)")
 
 // MARK: GO FURTHER _ Make your own Optionals
 //The struct creates a new data type called "User"
@@ -68,3 +69,18 @@ var user2:User?
 user2 = User(first:"Barb", balance:10.0)
 //TODO: remove the comment on the next line and fix the ERRORs that it causes
 //user2.makePayment(amount: 5.0).uppercased()
+
+
+
+/*#############################
+ 
+ */
+var num: String = "bleh"
+var number: Int? = Int(num)
+
+if number != nil {
+    print(number!)
+} else {
+    print("not a number!")
+}
+//if its nil
